0a8286e6f51d5b6eb096812da3cd86a87cee31de
raushan <raushan.raman23011999@gmail.com>
2019-08-18 22:25:33 +0530

    first commit

---
+++ water_melon.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+   long long int a;
+   cin>>a;
+   if(a%2==0){
+       if(a<=2){
+               cout<<"NO"<<endl;
+
+       }
+       else{
+             cout<<"YES"<<endl;
+       }
+   }
+   else{
+        cout<<"NO"<<endl;
+   }
+  return 0;
+}
