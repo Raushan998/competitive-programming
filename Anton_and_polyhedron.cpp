cba9ae2c8bff4a3c50f4f58222b7c7d3fd0e6d05
raushan <raushan.raman23011999@gmail.com>
2019-09-02 23:41:32 +0530

    first commit

---
+++ Anton_and_polyhedron.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    int sum=0;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s=="Tetrahedron")
+            sum+=4;
+        else if(s=="Cube")
+            sum+=6;
+        else if(s=="Octahedron")
+            sum+=8;
+        else if(s=="Dodecahedron")
+            sum+=12;
+        else if(s=="Icosahedron")
+            sum+=20;
+    }
+    cout<<sum<<endl;
+}
